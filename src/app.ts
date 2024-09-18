9a8eefe668925a5c1657b3fb9e4cc7aefcee91df
JucierG <jucier.gregorio05@gmail.com>
2024-09-18 16:52:14 -0300

    refactor: melhoria na estrutura do código das funções css, html e ts

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// importar data.json
+// importar entities/product.ts
+// criar os objetos de produto utilizando os dados de data.json
+
+import { Product } from "./entities/product";
+import data from "../data.json"
+
