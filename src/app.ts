0f257b62d95e829ce35200e75b8e8a9821152b8d
JucierG <jucier.gregorio05@gmail.com>
2024-09-30 16:45:55 -0300

    Commit Temporary

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,11 @@ import { Product } from "./entities/product";
 import data from "../data.json";
 import { Cart } from "./entities/cart";
 
-const product1 = new Product("Banana", "Fruta", 10, "http://example.com");
-product1.incrementQuantity();
-product1.incrementQuantity();
-product1.incrementQuantity();
-
-const product2 = new Product("Maçã", "Fruta", 5, "http://example.com");
-product2.incrementQuantity();
-product2.incrementQuantity();
-
-console.log(Cart);
-
-Cart.removeProduct(product2)
-
-console.log(Cart);
+for (const product of data) {
+    new Product(
+        product.name,
+        product.category,
+        product.price,
+        product.image.desktop,
+    ).toHTML();
+}
--- src/entities/product.ts
+++ src/entities/product.ts
@@ -17,6 +17,29 @@ export class Product {
     this._imageUrl = imageUrl;
   }
 
+  toHTML(){
+    const productListHTML = document.getElementById("product-list");
+    const productHTML = document.createElement("li");
+    productHTML.id = this._id
+
+    productHTML.innerHTML = `
+            <div class="rounded-xl overflow-hidden flex flex-col border border-black h-[270px] w-[250px]">
+          <div class="mb-10 relative h-full bg-red-500">
+            <div class="">img</div>
+            <button type="button" class="button rounded-lg text-white font-medium text-xs bg-red-400">Add to Cart</button>
+          </div>
+
+           <div class="flex flex-col">
+            <span class="product-category">${this._category}</span>
+            <span class="product-name text-xl font-medium">${this._name}</span>
+            <span class="product-price">${this._price}</span>
+          </div>
+        </div>
+    `
+    const buttonAddToCartHTML
+    productListHTML.appendChild(productHTML);
+  }
+
   get id(){
     return this._id;
   }
